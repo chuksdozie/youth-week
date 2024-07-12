6c634138ac839122270e5cb916880936b5387704
chuks dozie <chuksdozie48@gmail.com>
2024-07-12 05:45:00 +0100

    sponsor

--- src/constants/links.js
+++ src/constants/links.js
@@ -4,7 +4,7 @@ export const links = {
   feedback: "https://forms.gle/yeHeEWza9LriF1qd8",
   channel: "https://whatsapp.com/channel/0029VaDQIrD9RZAcIbQIEo3n",
   timeout: "https://timeout.devchuks.com",
-  sponsor: "https://timeout.devchuks.com",
+  sponsor: "https://paystack.com/pay/timeout_sponsor",
   tiktok: "https://www.tiktok.com/@iamchuksdozie",
   facebook: "https://www.facebook.com/tech4dev",
 };
